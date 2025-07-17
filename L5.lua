1b0c6beb2699f91e516a7b1a1e46a3c16ec58848
lee2sman <leetusman@gmail.com>
2025-07-17 16:14:28 +0200

    add html color table names and separate out toColor

--- L5.lua
+++ L5.lua
@@ -57,12 +57,12 @@ function love.draw()
   frameCount=frameCount+1
   local isPressed = love.mouse.isDown(1)
     
-    if isPressed and not wasPressed then
-        -- Mouse was just pressed this frame
-	if mousePressed ~= nil then mousePressed() end
-    end
+  if isPressed and not wasPressed then
+    -- Mouse was just pressed this frame
+    if mousePressed ~= nil then mousePressed() end
+  end
     
-    wasPressed = isPressed
+  wasPressed = isPressed
 
   if draw ~= nil then draw() end
 end
@@ -104,6 +104,28 @@ end
 
 function environment() 
   --background(255,255,255)
+end
+
+function toColor(_r,_g,_b,_a)
+  if _g == nil then
+    -- one argument = grayscale or color name
+    if type(_r) == "number" then
+      _r,_g,_b,_a = _r,_r,_r,255
+    elseif type(_r) == "string" then
+      _r,_g,_b=table.unpack(htmlColors[_r])
+      _a = 255
+    else
+      --ERROR
+    end
+  elseif _b == nil then
+    -- two arguments = grayscale, alpha
+    _a = _g
+    _r,_g,_b = _r,_r,_r
+  elseif _a == nil then
+    -- three arguments = r,g,b
+    _a = 255
+  end
+  return {_r/255,_g/255,_b/255,_a/255}
 end
 
 function defaults()
@@ -230,25 +252,25 @@ function rect(_a,_b,_c,_d)
   if global_rect_mode=="CORNERS" then --x1,y1,x2,y2
     love.graphics.rectangle(fillMode,_a,_b,_c-_a,_d-_b)
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.rectangle("line",_a,_b,_c-_a,_d-_b)
     love.graphics.setColor(r, g, b, a)
   elseif global_rect_mode=="CENTER" then --x-w/2,y-h/2,w,h 
     love.graphics.rectangle(fillMode, _a-_c/2,_b-_d/2,_c,_d)
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.rectangle("line", _a-_c/2,_b-_d/2,_c,_d)
     love.graphics.setColor(r, g, b, a)
   elseif global_rect_mode=="RADIUS" then --x-w/2,y-h/2,r1*2,r2*2
     love.graphics.rectangle(fillMode, _a-_c/2,_b-_d/2,_c*2,_d*2)
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.rectangle("line", _a-_c/2,_b-_d/2,_c*2,_d*2)
     love.graphics.setColor(r, g, b, a)
   else --CORNER default x,y,w,h
     love.graphics.rectangle(fillMode,_a,_b,_c,_d)
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.rectangle("line",_a,_b,_c,_d)
     love.graphics.setColor(r, g, b, a)
   end
@@ -259,19 +281,19 @@ function square(_a,_b,_c)
   if global_rect_mode=="CENTER" then --x-w/2,y-h/2,w,h 
     love.graphics.rectangle(fillMode, _a-_c/2,_b-_c/2,_c,_c)
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.rectangle("line", _a-_c/2,_b-_c/2,_c,_c)
     love.graphics.setColor(r, g, b, a)
   elseif global_rect_mode=="RADIUS" then --x-w/2,y-h/2,r*2,r*2
     love.graphics.rectangle(fillMode, _a-_c/2,_b-_c/2,_c*2,_c*2)
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.rectangle("line", _a-_c/2,_b-_c/2,_c*2,_c*2)
     love.graphics.setColor(r, g, b, a)
   else --CORNER default x,y,w,h
     love.graphics.rectangle(fillMode,_a,_b,_c,_c)
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.rectangle("line",_a,_b,_c,_c)
     love.graphics.setColor(r, g, b, a)
   end
@@ -282,25 +304,25 @@ function ellipse(_a,_b,_c,_d)
   if global_ellipse_mode=="RADIUS" then
     love.graphics.ellipse(fillMode,_a,_b,_c,_d)
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.ellipse("line",_a,_b,_c,_d)
     love.graphics.setColor(r, g, b, a)
   elseif global_ellipse_mode=="CORNER" then
     love.graphics.ellipse(fillMode,_a+_c/2,_b+_d/2,_c/2,_d/2)
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.ellipse("line",_a+_c/2,_b+_d/2,_c/2,_d/2)
     love.graphics.setColor(r, g, b, a)
   elseif global_ellipse_mode=="CORNERS" then
     love.graphics.ellipse(fillMode,_a+(_c-_a)/2,_b+(_d-_a)/2,(_c-_a)/2,(_d-_b)/2)
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.ellipse("line",_a+(_c-_a)/2,_b+(_d-_a)/2,(_c-_a)/2,(_d-_b)/2)
     love.graphics.setColor(r, g, b, a)
   else --default CENTER x,y,w/2,h/2
     love.graphics.ellipse(fillMode,_a,_b,_c/2,_d/2)
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.ellipse("line",_a,_b,_c/2,_d/2)
     love.graphics.setColor(r, g, b, a)
   end
@@ -310,7 +332,7 @@ function circle(_a,_b,_c)
 --love.graphics.ellipse( mode, x, y, radiusx, radiusy, segments )
   love.graphics.ellipse(fillMode,_a,_b,_c/2,_c/2)
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.ellipse("line",_a,_b,_c/2,_c/2)
     love.graphics.setColor(r, g, b, a)
 end
@@ -319,7 +341,7 @@ function quad(_x1,_y1,_x2,_y2,_x3,_y3,_x4,_y4) --this is a 4-sided love2d polygo
   --for other # of sides, use processing api call createShape
   love.graphics.polygon(fillMode,_x1,_y1,_x2,_y2,_x3,_y3,_x4,_y4)
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.polygon("line",_x1,_y1,_x2,_y2,_x3,_y3,_x4,_y4)
     love.graphics.setColor(r, g, b, a)
 end
@@ -327,7 +349,7 @@ end
 function triangle(_x1,_y1,_x2,_y2,_x3,_y3) --this is a 3-sided love2d polygon 
   love.graphics.polygon(fillMode,_x1,_y1,_x2,_y2,_x3,_y3)
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.polygon("line",_x1,_y1,_x2,_y2,_x3,_y3)
     love.graphics.setColor(r, g, b, a)
 end
@@ -338,13 +360,13 @@ function arc(_x,_y,_w,_h,_start,_stop,_arctype)
   if _arctype then
     love.graphics.arc(fillMode, _arctype, _x, _y, _w/2, _start, _stop)
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.arc("line", _arctype, _x, _y, _w/2, _start, _stop)
     love.graphics.setColor(r, g, b, a)
   else --no specified mode, use PIE default
     love.graphics.arc(fillMode, _x, _y, _w/2, _start, _stop)
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.arc("line", _x, _y, _w/2, _start, _stop)
     love.graphics.setColor(r, g, b, a)
   end
@@ -358,44 +380,170 @@ end
 function line(_x1,_y1,_x2,_y2)
   --a line is drawn in the stroke color
     local r, g, b, a = love.graphics.getColor()
-    fill(table.unpack(global_stroke_color))
+    love.graphics.setColor(table.unpack(global_stroke_color)) 
     love.graphics.line(_x1,_y1,_x2,_y2)
     love.graphics.setColor(r, g, b, a)
 end
 
 function background(_r,_g,_b,_a)
-    if _g == nil then
-        -- One argument = grayscale
-	_r,_g,_b,_a=_r,_r,_r,255
-    elseif _b == nil then
-	-- two arguments = grayscale, opacity
-	_a = _g
-	_r,_g,_b=_r,_r,_r
-    elseif _a == nil then
-	-- three arguments = r,g,b
-	_a=255
-    else
-        -- Four arguments = RGBA (assume 0-255 range)
-    end
-
-  love.graphics.clear(_r/255,_g/255,_b/255,_a/255)
+  love.graphics.clear(table.unpack(toColor(_r,_g,_b,_a)))
 end
 
 function fill(_r,_g,_b,_a)
-  if _g == nil then
-    -- one argument = grayscale
-    _r,_g,_b,_a = _r,_r,_r,255
-  elseif _b == nil then
-    -- two arguments = grayscale, alpha
-    _a = _g
-    _r,_g,_b = _r,_r,_r
-  elseif _a == nil then
-    -- three arguments = r,g,b
-    _a = 255
-  end
-  --love.graphics.setColor(love.math.colorFromBytes(_r, _g, _b,_a))
-  love.graphics.setColor(_r/255,_g/255,_b/255,_a/255)
+  love.graphics.setColor(table.unpack(toColor(_r,_g,_b,_a)))
 end
+
+----------------------- COLOR ------------------------
+htmlColors = {
+    ["aliceblue"] = {240, 248, 255},
+    ["antiquewhite"] = {250, 235, 215},
+    ["aqua"] = {0, 255, 255},
+    ["aquamarine"] = {127, 255, 212},
+    ["azure"] = {240, 255, 255},
+    ["beige"] = {245, 245, 220},
+    ["bisque"] = {255, 228, 196},
+    ["black"] = {0, 0, 0},
+    ["blanchedalmond"] = {255, 235, 205},
+    ["blue"] = {0, 0, 255},
+    ["blueviolet"] = {138, 43, 226},
+    ["brown"] = {165, 42, 42},
+    ["burlywood"] = {222, 184, 135},
+    ["cadetblue"] = {95, 158, 160},
+    ["chartreuse"] = {127, 255, 0},
+    ["chocolate"] = {210, 105, 30},
+    ["coral"] = {255, 127, 80},
+    ["cornflowerblue"] = {100, 149, 237},
+    ["cornsilk"] = {255, 248, 220},
+    ["crimson"] = {220, 20, 60},
+    ["cyan"] = {0, 255, 255},
+    ["darkblue"] = {0, 0, 139},
+    ["darkcyan"] = {0, 139, 139},
+    ["darkgoldenrod"] = {184, 134, 11},
+    ["darkgray"] = {169, 169, 169},
+    ["darkgreen"] = {0, 100, 0},
+    ["darkgrey"] = {169, 169, 169},
+    ["darkkhaki"] = {189, 183, 107},
+    ["darkmagenta"] = {139, 0, 139},
+    ["darkolivegreen"] = {85, 107, 47},
+    ["darkorange"] = {255, 140, 0},
+    ["darkorchid"] = {153, 50, 204},
+    ["darkred"] = {139, 0, 0},
+    ["darksalmon"] = {233, 150, 122},
+    ["darkseagreen"] = {143, 188, 139},
+    ["darkslateblue"] = {72, 61, 139},
+    ["darkslategray"] = {47, 79, 79},
+    ["darkslategrey"] = {47, 79, 79},
+    ["darkturquoise"] = {0, 206, 209},
+    ["darkviolet"] = {148, 0, 211},
+    ["deeppink"] = {255, 20, 147},
+    ["deepskyblue"] = {0, 191, 255},
+    ["dimgray"] = {105, 105, 105},
+    ["dimgrey"] = {105, 105, 105},
+    ["dodgerblue"] = {30, 144, 255},
+    ["firebrick"] = {178, 34, 34},
+    ["floralwhite"] = {255, 250, 240},
+    ["forestgreen"] = {34, 139, 34},
+    ["fuchsia"] = {255, 0, 255},
+    ["gainsboro"] = {220, 220, 220},
+    ["ghostwhite"] = {248, 248, 255},
+    ["gold"] = {255, 215, 0},
+    ["goldenrod"] = {218, 165, 32},
+    ["gray"] = {128, 128, 128},
+    ["green"] = {0, 128, 0},
+    ["greenyellow"] = {173, 255, 47},
+    ["grey"] = {128, 128, 128},
+    ["honeydew"] = {240, 255, 240},
+    ["hotpink"] = {255, 105, 180},
+    ["indianred"] = {205, 92, 92},
+    ["indigo"] = {75, 0, 130},
+    ["ivory"] = {255, 255, 240},
+    ["khaki"] = {240, 230, 140},
+    ["lavender"] = {230, 230, 250},
+    ["lavenderblush"] = {255, 240, 245},
+    ["lawngreen"] = {124, 252, 0},
+    ["lemonchiffon"] = {255, 250, 205},
+    ["lightblue"] = {173, 216, 230},
+    ["lightcoral"] = {240, 128, 128},
+    ["lightcyan"] = {224, 255, 255},
+    ["lightgoldenrodyellow"] = {250, 250, 210},
+    ["lightgray"] = {211, 211, 211},
+    ["lightgreen"] = {144, 238, 144},
+    ["lightgrey"] = {211, 211, 211},
+    ["lightpink"] = {255, 182, 193},
+    ["lightsalmon"] = {255, 160, 122},
+    ["lightseagreen"] = {32, 178, 170},
+    ["lightskyblue"] = {135, 206, 250},
+    ["lightslategray"] = {119, 136, 153},
+    ["lightslategrey"] = {119, 136, 153},
+    ["lightsteelblue"] = {176, 196, 222},
+    ["lightyellow"] = {255, 255, 224},
+    ["lime"] = {0, 255, 0},
+    ["limegreen"] = {50, 205, 50},
+    ["linen"] = {250, 240, 230},
+    ["magenta"] = {255, 0, 255},
+    ["maroon"] = {128, 0, 0},
+    ["mediumaquamarine"] = {102, 205, 170},
+    ["mediumblue"] = {0, 0, 205},
+    ["mediumorchid"] = {186, 85, 211},
+    ["mediumpurple"] = {147, 112, 219},
+    ["mediumseagreen"] = {60, 179, 113},
+    ["mediumslateblue"] = {123, 104, 238},
+    ["mediumspringgreen"] = {0, 250, 154},
+    ["mediumturquoise"] = {72, 209, 204},
+    ["mediumvioletred"] = {199, 21, 133},
+    ["midnightblue"] = {25, 25, 112},
+    ["mintcream"] = {245, 255, 250},
+    ["mistyrose"] = {255, 228, 225},
+    ["moccasin"] = {255, 228, 181},
+    ["navajowhite"] = {255, 222, 173},
+    ["navy"] = {0, 0, 128},
+    ["oldlace"] = {253, 245, 230},
+    ["olive"] = {128, 128, 0},
+    ["olivedrab"] = {107, 142, 35},
+    ["orange"] = {255, 165, 0},
+    ["orangered"] = {255, 69, 0},
+    ["orchid"] = {218, 112, 214},
+    ["palegoldenrod"] = {238, 232, 170},
+    ["palegreen"] = {152, 251, 152},
+    ["paleturquoise"] = {175, 238, 238},
+    ["palevioletred"] = {219, 112, 147},
+    ["papayawhip"] = {255, 239, 213},
+    ["peachpuff"] = {255, 218, 185},
+    ["peru"] = {205, 133, 63},
+    ["pink"] = {255, 192, 203},
+    ["plum"] = {221, 160, 221},
+    ["powderblue"] = {176, 224, 230},
+    ["purple"] = {128, 0, 128},
+    ["rebeccapurple"] = {102, 51, 153},
+    ["red"] = {255, 0, 0},
+    ["rosybrown"] = {188, 143, 143},
+    ["royalblue"] = {65, 105, 225},
+    ["saddlebrown"] = {139, 69, 19},
+    ["salmon"] = {250, 128, 114},
+    ["sandybrown"] = {244, 164, 96},
+    ["seagreen"] = {46, 139, 87},
+    ["seashell"] = {255, 245, 238},
+    ["sienna"] = {160, 82, 45},
+    ["silver"] = {192, 192, 192},
+    ["skyblue"] = {135, 206, 235},
+    ["slateblue"] = {106, 90, 205},
+    ["slategray"] = {112, 128, 144},
+    ["slategrey"] = {112, 128, 144},
+    ["snow"] = {255, 250, 250},
+    ["springgreen"] = {0, 255, 127},
+    ["steelblue"] = {70, 130, 180},
+    ["tan"] = {210, 180, 140},
+    ["teal"] = {0, 128, 128},
+    ["thistle"] = {216, 191, 216},
+    ["tomato"] = {255, 99, 71},
+    ["turquoise"] = {64, 224, 208},
+    ["violet"] = {238, 130, 238},
+    ["wheat"] = {245, 222, 179},
+    ["white"] = {255, 255, 255},
+    ["whitesmoke"] = {245, 245, 245},
+    ["yellow"] = {255, 255, 0},
+    ["yellowgreen"] = {154, 205, 50}
+}
 
 function rectMode(_mode)
   global_rect_mode=_mode
@@ -418,18 +566,7 @@ function strokeWeight(_w)
 end
 
 function stroke(_r,_g,_b,_a)
-  if _g == nil then
-    -- one argument = grayscale
-    _r,_g,_b,_a = _r,_r,_r,255
-  elseif _b == nil then
-    -- two arguments = grayscale, alpha
-    _a = _g
-    _r,_g,_b = _r,_r,_r
-  elseif _a == nil then
-    -- three arguments = r,g,b
-    _a = 255
-  end
-  global_stroke_color={_r,_g,_b,_a}
+  global_stroke_color = toColor(_r,_g,_b,_a)
 end
 
 function noStroke()
